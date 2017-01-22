c9098a6a18d4670f4d1673bba76cde4f83df8418
Brita Pentšuk <bpentsuk@itcollege.ee>
2017-01-22 14:30:17 +0200

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Laevade pommitamine!");
+	System.out.println("Brita Pentðuk");
+	System.out.println("I200");
+    }
+
+}
